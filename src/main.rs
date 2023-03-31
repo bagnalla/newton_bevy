fa47a4d64f7a8c65b0c0b1370caaba22f5dd6cd6
Alex <ab667712@ohio.edu>
2023-03-30 23:03:13 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -60,7 +60,8 @@ fn setup_light(mut commands: Commands) {
 
 fn setup_camera(mut commands: Commands) {
     commands.spawn_bundle(PerspectiveCameraBundle {
-        transform: Transform::from_xyz(0.0, 0.0, 5.0)
+        // transform: Transform::from_xyz(0.0, 0.0, 5.0)
+	transform: Transform::from_xyz(0.0, 0.0, 25.0)
             .looking_at(Vec3::ZERO, Vec3::Y),
         ..Default::default()
     });
